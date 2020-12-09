8a29de693f10d157b2384b8d69e8dad147b9f885
Isabelle <isa_ozawa@hotmail.com>
2020-12-09 10:55:45 -0300

    Projeto Spring RaizAgro

---
+++ raizAgro/raizAgro/src/main/java/com/raizagro/raizAgro/RaizAgroApplication.java
@@ -0,0 +1,13 @@
+package com.raizagro.raizAgro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizAgroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizAgroApplication.class, args);
+	}
+
+}
---
+++ raizAgro/raizAgro/src/test/java/com/raizagro/raizAgro/RaizAgroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raizagro.raizAgro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizAgroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
