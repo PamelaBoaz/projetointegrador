1c0467856c207a6f254a409ae5b3ad4de4dbe222
Witer <witer_m19@hotmail.com>
2021-01-07 18:37:36 -0300

    Atualizando

--- raizAgro/raizAgro/src/main/java/com/raizagro/raizAgro/model/Postagem.java
+++ raizAgro/raizAgro/src/main/java/com/raizagro/raizAgro/model/Postagem.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -32,9 +31,6 @@ public class Postagem {
 
 	@NotNull
 	private String descricao;
-
-	@Lob
-	private byte[] foto;
 
 	@NotNull
 	private String localizacao;
@@ -77,14 +73,6 @@ public class Postagem {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
-	}
-
-	public byte[] getFoto() {
-		return foto;
-	}
-
-	public void setFoto(byte[] foto) {
-		this.foto = foto;
 	}
 
 	public String getLocalizacao() {
